4459a43af21cda65599f64a5e39c188c63dccbbe
Carlos Beltran <beltranbot@gmail.com>
2019-01-27 18:25:16 -0500

    campos para filtros

--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -14,8 +14,13 @@ class BookList extends Component {
     books: [],
     authors: [],
     categories: [],
-    count: 0,
-    query_titulo: ''
+    query_titulo: '',
+    query_subtitulo: '',
+    query_fecha_publicacion: '',
+    query_descripcion: '',
+    query_editorial: '',
+    query_autor: '',
+    query_categoria: ''
   }
 
   async loadData () {
@@ -28,12 +33,97 @@ class BookList extends Component {
     response = await axios.get(editorials_json_url)
     let editorials = response.data
 
-    let {query_titulo} = this.state
+    let {
+      query_titulo,
+      query_subtitulo,
+      query_fecha_publicacion,
+      query_descripcion,
+      query_editorial,
+      query_autor,
+      query_categoria,
+    } = this.state
 
-    books = books.filter(author => {
+    books = books.filter(book => {
+      let editorial = ''
+      if (book.editorial) {
+        editorial = editorials.filter(editorial => editorial.id === book.editorial)[0]
+        editorial = editorial ? editorial.nombre : null
+      }
+
+      let autores = []
+      if (book.autores.length > 0) {
+        autores = authors.filter(author => book.autores.reduce((a, c) => {
+          return a || c === author.id
+        }, false))
+        if (autores) {
+          autores = autores.map(author => author.nombre)
+        }
+      }
+
+      let categorias = []
+      if (book.categorias.length > 0) {
+        categorias = categories.filter(category => book.categorias.reduce((a, c) => {
+          return a || c === category.id
+        }, false))
+        if (categorias) {
+          categorias = categorias.map(category => category.nombre)
+        }
+      }
+
       let result = []
       if (query_titulo.length > 0) {
-        result.push(author.titulo.includes(query_titulo))
+        result.push(book.titulo.toLowerCase().includes(query_titulo.toLowerCase()))
+      }
+      if (query_subtitulo.length > 0) {
+        result.push(book.subtitulo.toLowerCase().includes(query_subtitulo.toLowerCase()))
+      }
+      if (query_fecha_publicacion.length > 0) {
+        if (book.fecha_publicacion) {
+          result.push(book.fecha_publicacion.includes(query_fecha_publicacion))
+        } else {
+          result.push(false)
+        }
+      }
+      if (query_descripcion.length > 0) {
+        if (book.descripcion) {
+          result.push(book.descripcion.toLowerCase().includes(query_descripcion.toLowerCase()))
+        } else {
+          result.push(false)
+        }
+      }
+
+      if (query_editorial.length > 0) {
+        if (editorial) {
+          result.push(editorial.toLowerCase().includes(query_editorial.toLowerCase()))
+        } else {
+          result.push(false)
+        }
+      }
+
+      if (query_autor.length > 0) {
+        if (!book.autores) {
+          result.push(false) 
+        } else {
+          let local_result = autores.reduce(
+            (a, c) => a || c.toLowerCase().includes(query_autor.toLowerCase()),
+            false
+          )
+
+          result.push(local_result)
+        }
+      }
+
+      if (query_categoria.length > 0) {
+        if (!book.categorias) {
+          result.push(false) 
+        } else {
+          let local_result = categorias.reduce(
+            (a, c) => a || c.toLowerCase().includes(query_categoria.toLowerCase()),
+            false
+          )
+
+          result.push(local_result)
+        }
       }
 
       return result.reduce((a,c) => a && c, true)
@@ -61,11 +151,29 @@ class BookList extends Component {
     this.loadData()
   }
 
+  handleResetFilters = e => {
+    document.getElementById("query_form").reset()
+    let query_titulo = ''
+    let query_subtitulo = ''
+    let query_fecha_publicacion = ''
+    let query_descripcion = ''
+    let query_editorial = ''
+    this.setState({
+      query_titulo,
+      query_subtitulo,
+      query_fecha_publicacion,
+      query_descripcion,
+      query_editorial,
+    })
+    this.loadData()
+  }
+
   render() {
     let books = []
     let {authors, categories, editorials} = this.state
     if (this.state.books.length !== 0) {
       books = this.state.books.map(book => {
+
         let autores = book.autores.map(autor_id => {
           let autor = authors.filter(author => author.id === autor_id)[0]
           return (
@@ -110,19 +218,64 @@ class BookList extends Component {
           <Link to={'/books/create'}>Nuevo Libro</Link>
         </div>
         <div>
-        <form className="white" onSubmit={this.handleSubmit}>
-          <h5 className="grey-text text-darken-3">Filtros de Busqueda</h5>
-          <div className="input-field">
-            <label htmlFor="query_titulo">Titulo</label>
-            <input type="text"
-              id="query_titulo"
-              onChange={this.handleChange}
-            />
+          <form id="query_form" className="white" onSubmit={this.handleSubmit}>
+            <h5 className="grey-text text-darken-3">Filtros de Busqueda</h5>
             <div className="input-field">
-              <button className="btn pink lighten-1 z-depth-0">Filtrar</button>
+              <label htmlFor="query_titulo">Titulo</label>
+              <input type="text"
+                id="query_titulo"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+              <label htmlFor="query_subtitulo">Subtitulo</label>
+              <input type="text"
+                id="query_subtitulo"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+              <label htmlFor="query_fecha_publicacion">Fecha Publicación</label>
+              <input type="date"
+                id="query_fecha_publicacion"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+              <label htmlFor="query_descripcion">Descripción</label>
+              <input type="text"
+                id="query_descripcion"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+              <label htmlFor="query_editorial">Editorial</label>
+              <input type="text"
+                id="query_editorial"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+              <label htmlFor="query_autor">Autor</label>
+              <input type="text"
+                id="query_autor"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+              <label htmlFor="query_categoria">Categoría</label>
+              <input type="text"
+                id="query_categoria"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+                <button className="btn pink lighten-1 z-depth-0">Filtrar</button>
+            </div>          
+          </form>
+          <div className="input-field">
+              <button className="btn pink lighten-1 z-depth-0" onClick={this.handleResetFilters}>limpiar filtros</button>
           </div>
-          </div>
-        </form>
         </div>
         <table>
           <thead>
@@ -132,8 +285,8 @@ class BookList extends Component {
               <th>Subtitulo</th>
               <th>Fecha Publicación</th>
               <th>Autor(es)</th>
-              <th>Descripción</th>
               <th>Categoría(s)</th>
+              <th>Descripción</th>
               <th>Editorial</th>
             </tr>
           </thead>
